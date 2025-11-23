d86add093fc950b495bf404d1f7573830b3eb23c
rbrazier <19rbrazier@thelangton.org.uk>
2025-11-23 12:37:32 +0000

    pls work

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db= SQLAlchemy(app)
 
+
+class settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
